7d06481a368543347bd79a0204a83f8ee935794b
Dedaldino Papelo <dedaldino.papelo@okayulatech.ao>
2023-10-11 08:28:31 +0100

    fix

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -33,14 +33,10 @@ export class HttpService {
 
     return forkJoin({
       gameInfoRequest,
-/*       gameTrailersRequest,
-      gameScreenshotsRequest */
     }).pipe(
       map((resp: any) => {
         return {
           ...resp['gameInfoRequest'],
-/*           screenshots: resp['gameScreenshotsRequest']?.results,
-          trailers: resp['gameTrailersRequest']?.results, */
         };
       })
     );
